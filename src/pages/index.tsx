89f9a714bab233abbdbe002368bf98257375e677
Lizandro Daniel <ldaniel.eng@gmail.com>
2022-12-21 19:24:29 -0300

    Add Stitches, initial config for tests

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: "$rocketseat",
+  borderRadius: 6,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: "bold",
+  },
+
+  '&:hover':{
+    filter: 'brightness(0.8)',
+  }
+})
+
+export default function Home() {
+  return (
+    <Button><span>Teste</span> Enviar</Button>
+  )
+}
