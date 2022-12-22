94b6cdc18ff6b2ca559a4f7e93d2055b79dccebc
Lizandro Daniel <ldaniel.eng@gmail.com>
2022-12-22 11:25:00 -0300

    Created styles Global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,19 @@
 import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+
+import logoImage from '../assets/img/logo-ignite-shop.svg'
+import { Container, Header } from '../styles/pages/app'
+console.log(logoImage)
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImage.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
